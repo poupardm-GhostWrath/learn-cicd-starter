eb2e0435a283751abaedf189f1fc9553a255cffb
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-07-29 14:42:26 -0600

    Added Test

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	var header http.Header
+	var errMalformed = errors.New("malformed authorization header")
+
+	// Test: Good Header
+	header = http.Header{}
+	header.Add("Authorization", "ApiKey 123456")
+	key, err := GetAPIKey(header)
+	require.NoError(t, err)
+	assert.Equal(t, "12345", key)
+
+	// Test: No Header
+	header = http.Header{}
+	_, err = GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, ErrNoAuthHeaderIncluded, err)
+
+	// Test: Missing Key
+	header = http.Header{}
+	header.Add("Authorization", "Bearer 123456")
+	_, err = GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, errMalformed, err)
+
+	// Test: Malformed
+	header = http.Header{}
+	header.Add("Authorization", "ApiKey")
+	_, err = GetAPIKey(header)
+	require.Error(t, err)
+	assert.Equal(t, errMalformed, err)
+}
